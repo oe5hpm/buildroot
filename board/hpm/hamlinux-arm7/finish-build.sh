a70cd2ba62a5e6593e14a01e05f09581851b55c6
Hannes Petermaier <oe5hpm@oevsv.at>
2015-03-25 16:51:21 +0100

    hamlinux: first running setup on NAND board
    Signed-off-by: Hannes Petermaier <oe5hpm@oevsv.at>

--- board/hpm/hamlinux-arm7/finish-build.sh
+++ board/hpm/hamlinux-arm7/finish-build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 cp $1/rootfs.cpio.uboot /tftpboot/tseries
-[ -f $1/PPTImage.img.gz ] && rm $1/PPTImage.img.gz
-cp $1/rootfs.ext4.gz $1/PPTImage.img.gz
-pushd $1 >/dev/null
-md5sum PPTImage.img.gz > PPTImage.md5
-popd > /dev/null
+cp $1/rootfs.jffs2 /tftpboot/tseries
 
