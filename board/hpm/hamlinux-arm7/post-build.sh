3070794bf5a58891f2e5dbe979619265f4a623c3
Hannes Schmelzer <oe5hpm@oevsv.at>
2016-07-27 14:49:31 +0200

    hamlinux: drop version generator from post-build.script
    since there is already the information about build/date/... within /etc
    /os-release
    we don't need duplicate informations.
    
    Signed-off-by: Hannes Schmelzer <oe5hpm@oevsv.at>

--- board/hpm/hamlinux-arm7/post-build.sh
+++ board/hpm/hamlinux-arm7/post-build.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 TARGETDIR=$1
 BOARDDIR=$PWD/board/hpm/hamlinux-arm7
-COMMITID=`git describe --dirty | cut -d "-" -f 3-`
-BRANCH=`git branch | grep "*" | cut -d " " -f 2`
-DATUM=`date +%Y%m%d`
 
 rm -r -f $TARGETDIR/root/
 
 cp -r -f $BOARDDIR/rootfs-additions/* $TARGETDIR/
-
-echo "hamlinux (build: $BRANCH / $COMMITID) from $DATUM" > $TARGETDIR/versioninfo.txt
-
-cp $TARGETDIR/../.config $BOARDDIR/.config-old
-cp $TARGETDIR/../.config $BOARDDIR
 
 if [ $? -eq 0 ]; then
 	echo "$0 done successfully."
