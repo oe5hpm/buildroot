2dd6a11962fb91495e7e4708771cad0907e87877
Hannes Schmelzer <oe5hpm@oevsv.at>
2018-03-08 22:08:24 +0100

    hamlinux: drop finish-build actions
    Signed-off-by: Hannes Schmelzer <oe5hpm@oevsv.at>

--- board/hpm/hamlinux-arm7/finish-build.sh
+++ board/hpm/hamlinux-arm7/finish-build.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-DESTDIR=/tftpboot/tseries/hamlinux/
-cp $1/rootfs.jffs2 $DESTDIR
-cp $1/MLO $DESTDIR
-cp $1/u-boot.img $DESTDIR
-cp $1/zImage $DESTDIR
-cp $1/*.dtb $DESTDIR
-
-
+echo "finalizing .."
